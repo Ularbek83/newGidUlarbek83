f0c6fc59b4e82478e062b8426ddfca0e4764ed08
SuimbekovUlarbek <ularbel11111@mail.ru>
2024-05-06 06:32:31 +0600

    Initial Commit

---
+++ newGidUlarbek83/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newGidUlarbek83
+//
+//  Created by owner on 06.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
